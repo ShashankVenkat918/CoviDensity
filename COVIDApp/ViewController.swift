2bdec960d08925c03c33bbafeb62566f3977058a
Shashank Venkat <shashank.s.venkat@gmail.com>
2020-08-02 17:18:11 -0700

    Initial Commit

---
+++ COVIDApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  COVIDApp
+//
+//  Created by Shashank Venkat on 8/2/20.
+//  Copyright © 2020 Shashank Venkat. All rights reserved.
+//
+
+import UIKit
+import MapboxStatic
+import MapKit
+
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let camera = SnapshotCamera(
+            lookingAtCenter: CLLocationCoordinate2D(latitude: 45.52, longitude: -122.681944),
+            zoomLevel: 12)
+        let options = SnapshotOptions(
+            styleURL: URL(string: "mapbox://styles/shaw918/ckddp5s7c4beu1ilgo950t7ye")!,
+            camera: camera,
+            size: CGSize(width: 200, height: 200))
+        let snapshot = Snapshot(
+            options: options,
+            accessToken: "pk.eyJ1Ijoic2hhdzkxOCIsImEiOiJja2Rkb3FqMXoxOTFoMnhsMHpnZW9lMGU1In0.QWFbOxzLVW2UWdipU62d6A")
+        
+        imageView.image = snapshot.image
+    }
+
+
+    
+}
+
+
---
+++ COVIDAppTests/COVIDAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  COVIDAppTests.swift
+//  COVIDAppTests
+//
+//  Created by Shashank Venkat on 8/2/20.
+//  Copyright © 2020 Shashank Venkat. All rights reserved.
+//
+
+import XCTest
+@testable import COVIDApp
+
+class COVIDAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
